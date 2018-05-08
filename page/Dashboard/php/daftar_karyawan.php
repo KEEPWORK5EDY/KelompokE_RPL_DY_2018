89865951de90351254359a1cf6322f7cf20dd1c7
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-05-09 01:40:51 +0700

    Add Dashboard
    Tambah halaman dashboard

---
+++ page/Dashboard/php/daftar_karyawan.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+        <link rel="stylesheet" type="text/css" href="../assets/css/master.css">
+      </head>
+
+      <body class="tes">
+            <div class="row">
+                <div class="col-lg-12  main-chart">
+                    <!-- /row mt -->
+                    <div id=page>
+                      <section id="main-content">
+                          <section class="wrapper site-min-height">
+                          	<h3><i class="fa fa-angle-right"></i> List of Employees</h3><br>
+
+                         <div class="container">
+                           <div class="row">
+                             <div class="col-sm-4">
+                               <div class="panel panel-primary">
+                                 <div class="centered panel-heading">1</div>
+                                 <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                                 <div class="centered panel-footer">Buy 50 mobiles and get a gift card</div>
+                                 <!-- Trigger the modal with a button -->
+                                 <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+                         <!-- Modal -->
+                         <div class="modal fade" id="myModal" role="dialog">
+                           <div class="modal-dialog">
+                             <!-- Modal content-->
+                             <div class="modal-content">
+                               <div class="modal-header">
+                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                 <h4 class="modal-title">Modal Header</h4>
+                               </div>
+                               <div class="modal-body">
+                                 <?php include 'tampil.php'; ?>
+                               </div>
+                               <div class="modal-footer">
+                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                               </div>
+                             </div>
+                           </div>
+                         </div>
+         <!--yang bagus end-->
+                 </div>
+               </div>
+               <div class="col-sm-4">
+                 <div class="panel  panel-primary">
+                   <div class="centered panel-heading">2</div>
+                   <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                   <div class="centered panel-footer"><?php  ?></div>
+                   <button type="button" class="btn btn-xs">Edit</button>
+                 </div>
+               </div>
+               <div class="col-sm-4">
+                 <div class="panel  panel-primary">
+                   <div class="centered panel-heading">3</div>
+                   <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                   <div class="centered panel-footer">Buy 50 mobiles and get a gift card</div>
+                   <button type="button" class="btn btn-xs">Edit</button>
+                 </div>
+               </div>
+             </div>
+           </div><br>
+
+           <div class="container">
+             <div class="row">
+               <div class="col-sm-4">
+                 <div class="panel panel-primary">
+                   <div class="centered panel-heading">4</div>
+                   <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                   <div class="centered panel-footer">Buy 50 mobiles and get a gift card</div>
+                   <button type="button" class="btn btn-xs">Edit</button>
+                 </div>
+               </div>
+               <div class="col-sm-4">
+                 <div class="panel panel-primary">
+                   <div class="centered panel-heading">5</div>
+                   <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                   <div class="centered panel-footer">Buy 50 mobiles and get a gift card</div>
+                   <button type="button" class="btn btn-xs">Edit</button>
+                 </div>
+               </div>
+               <div class="col-sm-4">
+                 <div class="panel panel-primary">
+                   <div class="centered panel-heading">6</div>
+                   <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+                   <div class="centered panel-footer">Buy 50 mobiles and get a gift card</div>
+                   <button type="button" class="btn btn-xs">Edit</button>
+                 </div>
+               </div>
+
+
+             </div>
+           </div><br>
+
+                		</section><!--/wrapper -->
+                      </section><!-- /MAIN CONTENT -->
+                    </div>
+                </div>
+            </div>
+      </body>
+</html>
