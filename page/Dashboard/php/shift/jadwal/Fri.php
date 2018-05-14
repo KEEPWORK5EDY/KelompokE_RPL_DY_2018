4f42753cd563a22963decda5871c021323567cc2
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-05-15 04:13:52 +0700

    Php file hari untuk diload di modal shift/index.php

--- page/Dashboard/php/shift/jadwal/Fri.php
+++ page/Dashboard/php/shift/jadwal/Fri.php
@@ -1,17 +1,58 @@
-<?php
-     include "../../../../../php/connection.php";
+<head>
+     <link rel="stylesheet" href="../../../assets/css/home.css">
+</head>
 
-     $shift=sprintf("SELECT * FROM shift WHERE id_usaha=(SELECT id_usaha FROM usaha WHERE email='%s') AND hari='Fri'",$_SESSION["EPemilik"]);
-     //echo $shift;
-     $queryShift = mysqli_query($link,$shift);
+ <?php
+      include "../../../../../php/connection.php";
 
-     if(mysqli_num_rows($queryShift)>0){
-          while($jadwalShift = mysqli_fetch_array($queryShift)){
-               $id_shift=(string)$jadwalShift["id_shift"];
+      $shift=sprintf("SELECT * FROM shift WHERE id_usaha=(SELECT id_usaha FROM usaha WHERE email='%s') AND hari='Fri'",$_SESSION["EPemilik"]);
+      //echo $shift;
+      $queryShift = mysqli_query($link,$shift);
+      $tes=1;
+      if(mysqli_num_rows($queryShift)>0){
+           while($jadwalShift = mysqli_fetch_array($queryShift)){
+                //$tes=$jadwalShift["id_shift"];
+                $syntax= sprintf("SELECT * FROM pegawai WHERE  day5='%s'",$jadwalShift["id_shift"]);
+                //$syntax= sprintf(ddw"SELECT * FROM pegawai WHERE %s=1",$tes);
+                $query = mysqli_query($link,$syntax);
 
-               echo '<div id="'.$id_shift.'">
-                        <div>isi dari event abs circuit zik "'.$id_shift.'" "'.$jadwalShift["nama_shift"].'"</div>
-                    </div>';
-          }
-     }
+                     if(mysqli_num_rows($query)>0){
+                          echo '<div id="'.$jadwalShift["id_shift"].'">';
+                          echo "<div class='main-chart'>";
+                          echo "<div class='well'>";
+                          echo $jadwalShift['id_shift'];
+                          echo '<div class="centere">';
+                          while($data = mysqli_fetch_array($query)){ ?>
+
+                                      <div class="centere-profile">
+                                        <a  href="#"><img src="../../src/download.png" class="img-logo" width="60"></a>
+                                        <div class="centere-name">
+                                         <a href="#">
+                                              <?php    echo $data["nama"];
+                                                       echo $jadwalShift['id_shift'];
+                                                  ?>
+                                        </a>
+
+                                        </div>
+                                      </div>
+                                    <?php
+
+
+                          }
+                          echo "</div>";
+                          echo "</div>";
+                          echo "</div>";
+                          echo "</div>";
+
+                     }
+                     echo "<br>";
+           }
+                /*echo '<div id="'.$id_shift.'">
+                         <div>isi dari event abs circuit zik "'.$id_shift.'" "'.$jadwalShift["nama_shift"].'"</div>
+                         <div>jadwal "'.$jadwalShift["jam_mulai"].'" "'.$jadwalShift["jam_akhir"].'"</div>
+                     </div>';*/
+      }
+      /*echo '<div id="1">
+               <div>isi dari event abs circuit zik"'.$tes.'"</div>
+           </div>';*/
  ?>
