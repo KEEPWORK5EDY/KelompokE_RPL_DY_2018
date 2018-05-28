491b472357995f9ed1d3693d119dafef79b0689a
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-05-28 10:41:02 +0700

    create grafik data query

--- page/Dashboard/php/grafik/api.php
+++ page/Dashboard/php/grafik/api.php
@@ -1,8 +1,103 @@
 <?php
-     $arrLabels = array("January","February","March","April","May","June","July");
-     $arrDatasets = array('label' => "My First dataset",'fillColor' => "rgba(220,220,220,0.2)", 'strokeColor' => "rgba(220,220,220,1)", 'pointColor' => "rgba(220,220,220,1)", 'pointStrokeColor' => "#fff", 'pointHighlightFill' => "#fff", 'pointHighlightStroke' => "rgba(220,220,220,1)", 'data' => array('28', '48', '40', '19', '86', '27', '90'));
+     include "../../../../php/connection.php";
+     //$arrLabels = array("January","February","March","April","May","June","July");
+     //$arrDatasets = array('label' => "My First dataset",'fillColor' => "rgba(220,220,220,0.2)", 'strokeColor' => "rgba(220,220,220,1)", 'pointColor' => "rgba(220,220,220,1)", 'pointStrokeColor' => "#fff", 'pointHighlightFill' => "#fff", 'pointHighlightStroke' => "rgba(220,220,220,1)", 'data' => array('28', '48', '40', '19', '86', '27', '90'));
 
-     $arrReturn = array(array('labels' => $arrLabels, 'datasets' => $arrDatasets));
+     //$arrReturn = array(array('labels' => $arrLabels, 'datasets' => $arrDatasets));
 
-     print (json_encode($arrReturn));
+
+     $variable=$_POST["vard"];
+     $pilihan=$_POST["pilihan"];
+
+     // query max kehadiran tiap shift perhari
+     if(strcmp($variable,"day")==0){
+          //$array = array();
+          //print (json_encode($arrLabels));
+          $timezone = date_default_timezone_get();
+          //echo "The current server timezone is: " . $timezone;
+          $date = date('m/d/Y h:i:s a', time());
+          //echo "date : $date";
+          $dayD=date('Y-m-d');
+          //$d=spintf("%s",date("D",strtotime($t)));
+          $sd = $pilihan[0];
+          $dt = new DateTime($sd);
+          $date = $dt->format('l');
+
+          //$day=date("l",strtotime("-1 days"));
+          $day=date("l");
+          $array = array('data' =>array(), 'koordinat_y' => array(),'absensi' => array(), 'pilihan' => array());
+          array_push($array['pilihan'],$pilihan[0]);
+          array_push($array['pilihan'],$pilihan[1]);
+
+          $syntax=sprintf("SELECT id_shift,COUNT(id_shift) AS total FROM jadwal WHERE id_shift IN (SELECT id_shift FROM shift WHERE hari='%s' AND id_usaha=(SELECT id_usaha FROM usaha WHERE Email='%s')) group by id_shift",$day,$_SESSION["EPemilik"]);
+          $query=mysqli_query($link,$syntax);
+          if(mysqli_num_rows($query)>0){
+               while ($data=mysqli_fetch_array($query)) {
+                    array_push($array['data'],$data['total']);
+
+                    $syntaxS=sprintf("SELECT * FROM shift WHERE id_shift='%s'",$data['id_shift']);
+                    $queryS=mysqli_query($link,$syntaxS);
+                    if(mysqli_num_rows($queryS)>0){
+                         while ($dataS=mysqli_fetch_array($queryS)) {
+                              $syntaxA=sprintf("SELECT COUNT(%s) AS total FROM absensi WHERE id_shift='%s'",$data['id_shift'],$data['id_shift']);
+                              $queryA=mysqli_query($link,$syntaxA);
+                              while ($dataA=mysqli_fetch_array($queryA)){
+                                   array_push($array['absensi'],$dataA['total']);
+                              }
+                              array_push($array['koordinat_y'],$dataS['nama_shift']);
+                         }
+                    }
+                    //$array[]=$data['tes'];
+               }
+          }
+               //$array[]=$data['tes'];
+          print (json_encode($array));
+     }
+     else if(strcmp($variable,"week")==0){
+          $mon=date("Y-m-d", strtotime('monday this week'));
+          $sun=date("Y-m-d", strtotime('sunday this week'));
+          //$arrLabels = array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"), 'koordinat_y' => array(),'absensi' => array());
+
+          //$mon=date("l", strtotime('monday this week'));
+          //$d=spintf("%s",date("D",strtotime($t)));
+          $day=date("l");
+          $array = array('data' =>array(), 'koordinat_y' => array(),'absensi' => array());
+
+          $syntax=sprintf("SELECT sum(absensi) as total ,hari FROM (SELECT s.id_shift,hari,COUNT(j.id_shift) AS absensi FROM shift s,jadwal j WHERE s.id_shift=j.id_shift AND id_usaha=(SELECT id_usaha FROM usaha WHERE email='%s') GROUP BY s.id_shift) AS tes GROUP BY hari",$_SESSION["EPemilik"]);
+          $query=mysqli_query($link,$syntax);
+          if(mysqli_num_rows($query)>0){
+               while ($data=mysqli_fetch_array($query)){
+                    array_push($array['data'],$data['total']);
+                    array_push($array['koordinat_y'],$data['hari']);
+               }
+          }
+          for($a=0;$a<6;$a++){
+               for($b=0;$b<=6-$a-1;$b++){
+                     if(date("N",strtotime($array['koordinat_y'][$b]))>date("N",strtotime($array['koordinat_y'][$b+1]))){
+                             $tmp=$array['koordinat_y'][$b];
+                             $array['koordinat_y'][$b]=$array['koordinat_y'][$b+1];
+                             $array['koordinat_y'][$b+1]=$tmp;
+
+                             $tmp=$array['data'][$b];
+                             $array['data'][$b]=$array['data'][$b+1];
+                             $array['data'][$b+1]=$tmp;
+
+                             $tmp=$array['absensi'][$b];
+                             $array['absensi'][$b]=$array['absensi'][$b+1];
+                             $array['absensi'][$b+1]=$tmp;
+                     }
+                 }
+          }
+          $hari = array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+          for($a=0;$a<6;$a++){
+               if($array['koordinat_y'][$a]==null){
+                    $array['koordinat_y'][$a]=$hari[$a];
+               }
+          }
+               //$array[]=$data['tes'];
+          print(json_encode($array));
+     }
+     //query yang hadir
+     //select id_shift,count(id_shift) from absensi where id_shift in (select id_shift from shift where hari='Wednesday' and id_usaha=(select id_usaha from usaha where Email='zikri')) group by id_shift;
+
  ?>
--- page/Dashboard/php/grafik/kehadiran.php
+++ page/Dashboard/php/grafik/kehadiran.php
@@ -6,68 +6,72 @@
      <head>
           <meta charset="utf-8">
           <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-            <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+          <script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+          <script src="https://code.jquery.com/jquery-migrate-3.0.1.js"></script>
+
+          <link href="../../assets/k-bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+          <link href="../../assets/k-bootstrap/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+
+          <link rel="stylesheet" type="text/css" href="chart.css">
+          <link rel="stylesheet" type="text/css" href="button.css">
+          <link rel="stylesheet" type="text/css" href="style.css">
           <title></title>
      </head>
      <body>
-          <canvas id="myChart" width="400" height="400"></canvas>
-          <script>
-          $(document).ready(function() {
-                    $.ajax({
-                          type: 'POST',
-                          url: 'api.php',
-                          data:data,
-                          dataType: 'json',
-                          success: function (data) {
-                               alert(JSON.stringify(data));
-                               /*lineChartData = data;//alert(JSON.stringify(data));
-                               var myLine = new Chart(document.getElementById("myChart").getContext("2d")).Line(lineChartData);
+          <div class="top-menu">
+               <div class="col menu-col">
+                    <div class="menu">
+                         <h2>Pilihan</h2>
+                         <div class="dropdown">
+                         <button onclick="myFunction()" class="dropbtn">Variable</button>
+                           <div id="myDropdown" style="position: fixed" class="dropdown-content">
+                             <a id="day" onclick="">Day</a>
+                             <a id="week" onclick="">Weak</a>
+                             <a id="month" onclick="">Month</a>
+                             <a id="year" onclick="">Years</a>
+                           </div>
+                           <input id=textF style="width: 140px" class="dropbtn" type="text" name="" value="" readonly>
+                         </div>
+                    </div>
 
-                               var ctx = document.getElementById("canvas").getContext("2d");
-                               window.myLine = new Chart(ctx).Line(lineChartData, {responsive: true});
-                          */}
-                     });
-           });
-          //var ctx = document.getElementById("myChart").getContext('2d');
-          /*var myChart = new Chart(ctx, {
-              type: 'bar',
-              data: {
-                  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                  datasets: [{
-                      label: '# of Votes',
-                      data: [12, 19, 3, 5, 2, 3],
-                      backgroundColor: [
-                          'rgba(255, 99, 132, 0.2)',
-                          'rgba(54, 162, 235, 0.2)',
-                          'rgba(255, 206, 86, 0.2)',
-                          'rgba(75, 192, 192, 0.2)',
-                          'rgba(153, 102, 255, 0.2)',
-                          'rgba(255, 159, 64, 0.2)'
-                      ],
-                      borderColor: [
-                          'rgba(255,99,132,1)',
-                          'rgba(54, 162, 235, 1)',
-                          'rgba(255, 206, 86, 1)',
-                          'rgba(75, 192, 192, 1)',
-                          'rgba(153, 102, 255, 1)',
-                          'rgba(255, 159, 64, 1)'
-                      ],
-                      borderWidth: 1
-                  }]
-              },
-              options: {
-                  scales: {
-                      yAxes: [{
-                          ticks: {
-                              beginAtZero:true
-                          }
-                      }]
-                  }
-              }
-         });*/
+               </div>
+          <div>
+               <div class="form-group" style="width: 340px;height: 70px">
+                <label for="dtp_input2" class="col-md-2 control-label" style="width: 100%">Date Picking</label>
+                <div id=KW_SKalender class="input-group date form_date col-md-5" style="width: 75%" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_inputS" data-link-format="yyyy-mm-dd">
+                    <input class="form-control date" id="tglS" size="16" type="text" value="" readonly>
+                    <span class="input-group-addon"><span class="click glyphicon glyphicon-calendar"></span></span>
+                </div>
+                    <input  type="hidden" id="dtp_inputS" value="" readonly/><br/>
+               </div>
 
-          </script>
+               <div class="form-group" style="width: 340px;height: 70px">
+                <label for="dtp_input2" class="col-md-2 control-label" style="width: 100%">Date Picking</label>
+                <div id=KW_FKalender class="input-group date form_date col-md-5" style="width: 75%" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_inputF" data-link-format="yyyy-mm-dd">
+                    <input class="form-control date" id="tglF" size="16" type="text" value="" readonly>
+                    <span class="input-group-addon"><span class="click glyphicon glyphicon-calendar"></span></span>
+                </div>
+                    <input  type="hidden" id="dtp_inputF" value="" readonly/><br/>
+               </div>
+          </div>
+     </div>
 
+          <div class="wrapper">
+               <div id="chartHeader">
+                    <h4 id="textHeader">Log Absensi</h4>
+               </div>
+          </div>
 
+               <canvas id="Chart"></canvas>
+               <script src="grafik.js"></script>
+
+               <script type="text/javascript" src="../../assets/k-bootstrap/jquery/jquery-1.8.3.min.js" charset="UTF-8"></script>
+               <script type="text/javascript" src="../../assets/k-bootstrap/bootstrap/js/bootstrap.min.js"></script>
+               <script type="text/javascript" src="../../assets/k-bootstrap/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+               <script type="text/javascript" src="../../assets/k-bootstrap/js/locales/bootstrap-datetimepicker.fr.js" charset="UTF-8"></script>
+
+               <script type="text/javascript">
+
+               </script>
      </body>
 </html>
