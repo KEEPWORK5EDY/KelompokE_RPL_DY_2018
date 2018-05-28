5e7e025662a16f0a6e1cef4530d414cbdfb740dd
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-05-28 10:43:15 +0700

    edit dashboard

--- page/Dashboard/php/dashboard.php
+++ page/Dashboard/php/dashboard.php
@@ -110,7 +110,7 @@
 
                     </iframe>
                </div>-->
-               <div class="load">
+               <div id="loadC" class="load">
                    <iframe id=frame src="" width=100% height=100% marginwidth=0 marginheight=0 frameborder="0" scrolling="yes"></iframe>
                </div>
           </div>
