bffdc33078bbbf219ab358789f5bfb1787fc9ad7
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-05-29 21:25:26 +0700

    add css and js snake-bar , js drop and drag

---
+++ page/Dashboard/php/shift/js/snake.js
@@ -0,0 +1,15 @@
+function showS() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+
+    // setTimeout(function(){ x.className = x.className.replace("show", ""); },3000);
+}
+
+function closeS(){
+     //alert(4);
+     var x = document.getElementById("snackbar");
+     x.className = "close";
+         //tes();
+     //document.getElementById("snackbar").remove();
+     //setTimeout(function(){ x.className = x.className.replace("close", ""); },500);
+}
