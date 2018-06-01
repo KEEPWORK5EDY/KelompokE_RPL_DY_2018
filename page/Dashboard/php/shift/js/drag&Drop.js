0ff8cfc96d461358c073fd187540e241dc282c95
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-06-01 14:00:04 +0700

    edit file js drag and drop

--- page/Dashboard/php/shift/js/drag&Drop.js
+++ page/Dashboard/php/shift/js/drag&Drop.js
@@ -8,6 +8,33 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    if(ev.target.id.localeCompare("shift")==0){
+
+         var data = ev.dataTransfer.getData("text");
+
+         var x="div"+document.getElementById(data).id;
+         //alert(x);
+         ev.target.appendChild(document.getElementById(x));
+
+         var parent = document.getElementById("container-data");
+         var child = document.getElementById("grid"+x);
+         parent.removeChild(child);
+         //alert(ev.target.id);
+
+    }
+    else {
+
+         var data = ev.dataTransfer.getData("text");
+         alert(document.getElementById(data).id);
+         
+         var x="div"+document.getElementById(data).id;
+         //ev.target.appendChild(document.getElementById(x));
+         var grid=document.createElement("DIV");
+         grid.id="grid"+x;
+         grid.className="grid-item";
+         grid.appendChild(document.getElementById(x));
+         ev.target.appendChild(grid);
+         //alert(grid.id);
+
+    }
 }
